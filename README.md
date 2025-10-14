b8cc36b69c42222b0857bbbd31abf55de7aaf3a5
Gabi <124410388+Gabifern@users.noreply.github.com>
2025-10-14 09:45:40 -0400

    setup the name and age input and create ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Version-Control-Big-Ideas
 Lab Assignment for Skills Lab 6: Version Control
+
+Group Members:
+Gabi Fernandes
+
+What Our Program Does:
+
---
+++ do_something.py
@@ -0,0 +1,7 @@
+# It takes a name input
+name = input("Enter your name: ")
+
+# It takes some other input
+age = input("Enter your age: ")
+
+# It does Something™ with this other input
