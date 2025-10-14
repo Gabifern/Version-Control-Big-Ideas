fceb8af6eb6b176806968d983a3c270ecf61b46b
Thomas Harrison <145225655+Wryke@users.noreply.github.com>
2025-10-14 17:59:26 -0400

    Update do_something.py
    Thomas' contribution

--- do_something.py
+++ do_something.py
@@ -1,7 +1,14 @@
+currentYear = 2025
 # It takes a name input
 name = input("Enter your name: ")
+while(True):    
+    # It takes some other input
+    age = input("Enter your age: ")
 
-# It takes some other input
-age = input("Enter your age: ")
-
-# It does Something™ with this other input
+    # Attempts to take age input and convert to int
+    try:
+      age = int(age)
+      print(name, "was born in", currentYear - age);
+      break
+    except:
+      print("\"", age, "\" is not an integer!")
